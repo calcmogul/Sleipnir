cfe66a54ccf98492b6ac949cd2106270ce7ff970
Tyler Veness <calcmogul@gmail.com>
2025-01-28 22:19:14 -0800

    Use wpi::byteswap()

--- include/sleipnir/util/spy.hpp
+++ include/sleipnir/util/spy.hpp
@@ -12,6 +12,7 @@
 #include <string_view>
 
 #include <Eigen/SparseCore>
+#include <wpi/util/bit.hpp>
 
 namespace slp {
 
@@ -114,7 +115,7 @@ class Spy {
    */
   void write32le(int32_t num) {
     if constexpr (std::endian::native != std::endian::little) {
-      num = std::byteswap(num);
+      num = wpi::util::byteswap(num);
     }
     m_file.write(reinterpret_cast<char*>(&num), sizeof(num));
   }
