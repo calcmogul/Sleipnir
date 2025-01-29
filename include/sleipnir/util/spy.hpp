ceb988f2830d59ebdccdb3f8164048db33f5aaad
Tyler Veness <calcmogul@gmail.com>
2025-01-28 22:19:14 -0800

    Use wpi::byteswap()

--- include/sleipnir/util/spy.hpp
+++ include/sleipnir/util/spy.hpp
@@ -11,6 +11,7 @@
 
 #include <Eigen/SparseCore>
 #include <wpi/SmallVector.h>
+#include <wpi/bit.h>
 
 #include "sleipnir/util/symbol_exports.hpp"
 
@@ -114,7 +115,7 @@ class SLEIPNIR_DLLEXPORT Spy {
    */
   void write32le(int32_t num) {
     if constexpr (std::endian::native != std::endian::little) {
-      num = std::byteswap(num);
+      num = wpi::byteswap(num);
     }
     m_file.write(reinterpret_cast<char*>(&num), sizeof(num));
   }
