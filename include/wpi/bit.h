34349ddea7e10115895f450f2256eda655fcc1be
Tyler Veness <calcmogul@gmail.com>
2025-02-01 16:51:04 -0800

    Add patch generator and shims to make the project compile

---
+++ include/wpi/bit.h
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <array>
+#include <cstddef>
+
+namespace wpi {
+
+template <typename T>
+T byteswap(T n) noexcept {
+  auto value_representation =
+      std::bit_cast<std::array<std::byte, sizeof(int32_t)>>(n);
+  std::ranges::reverse(value_representation);
+  return std::bit_cast<int32_t>(value_representation);
+}
+
+}  // namespace wpi
