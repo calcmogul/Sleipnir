cc290b31b86de6e5b28f29b1501321b440a4a718
Tyler Veness <calcmogul@gmail.com>
2025-02-01 16:51:04 -0800

    Add patch generator and shims to make the project compile

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -9,7 +9,7 @@ macro(compiler_flags target)
     endif()
     set_property(TARGET ${target} PROPERTY COMPILE_WARNING_AS_ERROR ON)
 
-    target_compile_features(${target} PUBLIC cxx_std_23)
+    target_compile_features(${target} PUBLIC cxx_std_20)
     if(MSVC)
         target_compile_options(${target} PUBLIC /MP /Zf /utf-8 /bigobj)
     endif()
