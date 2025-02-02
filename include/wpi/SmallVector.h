c6207d81874ec7474efadf5f3a4a2a1cbb2c9888
Tyler Veness <calcmogul@gmail.com>
2025-02-01 16:51:04 -0800

    Add patch generator and shims to make the project compile

---
+++ include/wpi/SmallVector.h
@@ -0,0 +1,12 @@
+// Copyright (c) Sleipnir contributors
+
+#pragma once
+
+#include <sleipnir/util/small_vector.hpp>
+
+namespace wpi {
+
+template <typename T>
+using SmallVector = sleipnir::small_vector<T>;
+
+}  // namespace wpi
