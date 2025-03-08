7ccbb5a4a22be64ec0c66ac3a68e9f084dad115f
Tyler Veness <calcmogul@gmail.com>
2025-03-07 19:23:36 -0800

    Implement augmented Lagrangian solver

--- include/sleipnir/optimization/solver/util/kkt_error.hpp
+++ include/sleipnir/optimization/solver/util/kkt_error.hpp
@@ -149,4 +149,78 @@ Scalar kkt_error(const Eigen::Vector<Scalar, Eigen::Dynamic>& g,
   }
 }
 
+/// Returns the KKT error for the augmented Lagrangian method.
+///
+/// @tparam Scalar Scalar type.
+/// @tparam T The type of KKT error to compute.
+/// @param g Gradient of the cost function ∇f.
+/// @param A_e The problem's equality constraint Jacobian Aₑ(x) evaluated at the
+///     current iterate.
+/// @param c_e The problem's equality constraints cₑ(x) evaluated at the current
+///     iterate.
+/// @param A_i The problem's inequality constraint Jacobian Aᵢ(x) evaluated at
+///     the current iterate.
+/// @param c_i The problem's inequality constraints cᵢ(x) evaluated at the
+///     current iterate.
+/// @param y Equality constraint dual variables.
+/// @param z Inequality constraint dual variables.
+/// @param ρ The penalty parameter.
+/// @param a The inequality constraint active set.
+template <typename Scalar, KKTErrorType T>
+Scalar kkt_error(const Eigen::Vector<Scalar, Eigen::Dynamic>& g,
+                 const Eigen::SparseMatrix<Scalar>& A_e,
+                 const Eigen::Vector<Scalar, Eigen::Dynamic>& c_e,
+                 const Eigen::SparseMatrix<Scalar>& A_i,
+                 const Eigen::Vector<Scalar, Eigen::Dynamic>& c_i,
+                 const Eigen::Vector<Scalar, Eigen::Dynamic>& y,
+                 const Eigen::Vector<Scalar, Eigen::Dynamic>& z, Scalar ρ,
+                 const Eigen::Vector<Scalar, Eigen::Dynamic>& a) {
+  // The KKT conditions from docs/algorithms.md:
+  //
+  //   ∇f − Aₑᵀy − Aᵢᵀz = 0
+  //   zᵀcᵢ = 0
+  //   cₑ = 0
+  //   cᵢ ≥ 0
+
+  if constexpr (T == KKTErrorType::INF_NORM_SCALED) {
+    // See equation (5) of [2].
+
+    // s_d = max(sₘₐₓ, (‖y‖₁ + ‖z‖₁) / (m + n)) / sₘₐₓ
+    constexpr Scalar s_max(100);
+    Scalar s_d =
+        std::max(s_max, (y.template lpNorm<1>() + z.template lpNorm<1>()) /
+                            Scalar(y.rows() + z.rows())) /
+        s_max;
+
+    if (ρ == Scalar(0)) {
+      // s_c = max(sₘₐₓ, ‖z‖₁ / n) / sₘₐₓ
+      Scalar s_c =
+          std::max(s_max, z.template lpNorm<1>() / Scalar(z.rows())) / s_max;
+
+      // ‖∇f − Aₑᵀ(y − ρcₑ) − Aᵢᵀ(z − ρdiag(a)cᵢ)‖_∞ / s_d
+      // ‖zᵀcᵢ‖_∞ / s_c
+      // ‖cₑ‖_∞
+      // ‖min(cᵢ, 0)‖_∞
+      return std::max(
+          {(g - A_e.transpose() * y - A_i.transpose() * z)
+                   .template lpNorm<Eigen::Infinity>() /
+               s_d,
+           (z.transpose() * c_i).template lpNorm<Eigen::Infinity>() / s_c,
+           c_e.template lpNorm<Eigen::Infinity>(),
+           c_i.cwiseMin(Scalar(0)).template lpNorm<Eigen::Infinity>()});
+    } else {
+      return (g - A_e.transpose() * (y - ρ * c_e) -
+              A_i.transpose() * (z - ρ * a.asDiagonal() * c_i))
+                 .template lpNorm<Eigen::Infinity>() /
+             s_d;
+    }
+  } else if constexpr (T == KKTErrorType::ONE_NORM) {
+    return (g - A_e.transpose() * y - A_i.transpose() * z)
+               .template lpNorm<1>() +
+           (z.transpose() * c_i).template lpNorm<1>() +
+           c_e.template lpNorm<1>() +
+           c_i.cwiseMin(Scalar(0)).template lpNorm<1>();
+  }
+}
+
 }  // namespace slp
