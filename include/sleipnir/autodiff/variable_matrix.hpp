3b7d1a2abca6e247d9ad54f8939b57a68ef69512
Tyler Veness <calcmogul@gmail.com>
2025-03-07 19:23:36 -0800

    Implement augmented Lagrangian solver

--- include/sleipnir/autodiff/variable_matrix.hpp
+++ include/sleipnir/autodiff/variable_matrix.hpp
@@ -330,6 +330,21 @@ class SLEIPNIR_DLLEXPORT VariableMatrix {
   }
 
   /**
+   * Sets the VariableMatrix's internal values.
+   *
+   * @param values Diagonal matrix of values.
+   */
+  template <typename Derived>
+    requires std::same_as<typename Derived::Scalar, double>
+  void set_value(const Eigen::DiagonalBase<Derived>& values) {
+    slp_assert(rows() == values.rows() && cols() == values.cols());
+
+    for (int row = 0; row < std::min(values.rows(), values.cols()); ++row) {
+      (*this)[row, row].set_value(values.diagonal()[row]);
+    }
+  }
+
+  /**
    * Returns the element at the given row and column.
    *
    * @param row The row.
