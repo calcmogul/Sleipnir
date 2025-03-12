ffadb2d70dd047d7fced3e0762dae54abf2c7f83
Tyler Veness <calcmogul@gmail.com>
2025-03-11 22:06:23 -0700

    Implement log-domain interior-point method

--- src/optimization/solver/newton.cpp
+++ src/optimization/solver/newton.cpp
@@ -191,7 +191,7 @@ ExitStatus newton(
     user_callbacks_profiler.stop();
     ScopedProfiler linear_system_compute_profiler{linear_system_compute_prof};
 
-    // Solve the Newton-KKT system
+    // Factorize the Newton-KKT system
     //
     // Hpˣ = −∇f
     solver.compute(H);
@@ -199,6 +199,7 @@ ExitStatus newton(
     linear_system_compute_profiler.stop();
     ScopedProfiler linear_system_solve_profiler{linear_system_solve_prof};
 
+    // Solve the Newton-KKT system for the step
     Eigen::VectorXd p_x = solver.solve(-g);
 
     linear_system_solve_profiler.stop();
@@ -303,7 +304,7 @@ ExitStatus newton(
     if (options.diagnostics) {
       print_iteration_diagnostics(
           iterations, IterationType::NORMAL,
-          inner_iter_profiler.current_duration(), E_0, f.value(), 0.0, 0.0, 0.0,
+          inner_iter_profiler.current_duration(), E_0, f.value(), 0.0, 0.0,
           solver.hessian_regularization(), α, α_max, α_reduction_factor, 1.0);
     }
 #endif
