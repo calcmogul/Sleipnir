48c33f791c882ca5506d8fae2a304c45534e0328
Tyler Veness <calcmogul@gmail.com>
2025-03-11 22:06:23 -0700

    Implement log-domain interior-point method

--- include/sleipnir/optimization/solver/util/error_estimate.hpp
+++ include/sleipnir/optimization/solver/util/error_estimate.hpp
@@ -74,35 +74,39 @@ Scalar error_estimate(const Eigen::Vector<Scalar, Eigen::Dynamic>& g,
 ///     the current iterate.
 /// @param c_i The problem's inequality constraints cᵢ(x) evaluated at the
 ///     current iterate.
-/// @param s Inequality constraint slack variables.
 /// @param y Equality constraint dual variables.
-/// @param z Inequality constraint dual variables.
-/// @param μ Barrier parameter.
+/// @param v Log-domain variables.
+/// @param sqrt_μ Square root of the barrier parameter.
 template <typename Scalar>
 Scalar error_estimate(const Eigen::Vector<Scalar, Eigen::Dynamic>& g,
                       const Eigen::SparseMatrix<Scalar>& A_e,
                       const Eigen::Vector<Scalar, Eigen::Dynamic>& c_e,
                       const Eigen::SparseMatrix<Scalar>& A_i,
                       const Eigen::Vector<Scalar, Eigen::Dynamic>& c_i,
-                      const Eigen::Vector<Scalar, Eigen::Dynamic>& s,
                       const Eigen::Vector<Scalar, Eigen::Dynamic>& y,
-                      const Eigen::Vector<Scalar, Eigen::Dynamic>& z,
-                      Scalar μ) {
-  // Update the error estimate using the KKT conditions from equations (19.5a)
-  // through (19.5d) of [1].
+                      const Eigen::Vector<Scalar, Eigen::Dynamic>& v,
+                      Scalar sqrt_μ) {
+  // Update the error estimate using the KKT conditions.
   //
   //   ∇f − Aₑᵀy − Aᵢᵀz = 0
-  //   Sz − μe = 0
   //   cₑ = 0
   //   cᵢ − s = 0
   //
+  // where
+  //
+  //   s = √(μ)e⁻ᵛ
+  //   z = √(μ)eᵛ
+  //
   // The error tolerance is the max of the following infinity norms scaled by
-  // s_d and s_c (see equation (5) of [2]).
+  // s_d (see equation (5) of [2]).
   //
   //   ‖∇f − Aₑᵀy − Aᵢᵀz‖_∞ / s_d
-  //   ‖Sz − μe‖_∞ / s_c
   //   ‖cₑ‖_∞
   //   ‖cᵢ − s‖_∞
+
+  Eigen::Vector<Scalar, Eigen::Dynamic> s =
+      sqrt_μ * (-v).array().exp().matrix();
+  Eigen::Vector<Scalar, Eigen::Dynamic> z = sqrt_μ * v.array().exp().matrix();
 
   // s_d = max(sₘₐₓ, (‖y‖₁ + ‖z‖₁) / (m + n)) / sₘₐₓ
   constexpr Scalar s_max(100);
@@ -111,18 +115,9 @@ Scalar error_estimate(const Eigen::Vector<Scalar, Eigen::Dynamic>& g,
                           Scalar(y.rows() + z.rows())) /
       s_max;
 
-  // s_c = max(sₘₐₓ, ‖z‖₁ / n) / sₘₐₓ
-  Scalar s_c =
-      std::max(s_max, z.template lpNorm<1>() / Scalar(z.rows())) / s_max;
-
-  const auto S = s.asDiagonal();
-  const Eigen::Vector<Scalar, Eigen::Dynamic> μe =
-      Eigen::Vector<Scalar, Eigen::Dynamic>::Constant(s.rows(), μ);
-
   return std::max({(g - A_e.transpose() * y - A_i.transpose() * z)
                            .template lpNorm<Eigen::Infinity>() /
                        s_d,
-                   (S * z - μe).template lpNorm<Eigen::Infinity>() / s_c,
                    c_e.template lpNorm<Eigen::Infinity>(),
                    (c_i - s).template lpNorm<Eigen::Infinity>()});
 }
