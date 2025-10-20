11c9b61a5427cf3545a4041a0fe928b41fbf4130
Tyler Veness <calcmogul@gmail.com>
2025-10-19 17:44:32 -0700

    Hessian diagnostics

--- test/include/scalar_types_under_test.hpp
+++ test/include/scalar_types_under_test.hpp
@@ -2,12 +2,4 @@
 
 #pragma once
 
-#include <sleipnir/optimization/ocp.hpp>
-#include <sleipnir/optimization/problem.hpp>
-
-#include "explicit_double.hpp"
-
-extern template class slp::OCP<ExplicitDouble>;
-extern template class slp::Problem<ExplicitDouble>;
-
-#define SCALAR_TYPES_UNDER_TEST double, ExplicitDouble
+#define SCALAR_TYPES_UNDER_TEST double
--- test/src/scalar_types_under_test.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Sleipnir contributors
-
-#include "scalar_types_under_test.hpp"
-
-#include "explicit_double.hpp"
-
-template class slp::OCP<ExplicitDouble>;
-template class slp::Problem<ExplicitDouble>;
