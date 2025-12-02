79ec1a45b58472ff34f409a02017a48fec1aa36c
Tyler Veness <calcmogul@gmail.com>
2025-12-01 19:26:04 -0800

    Use replicated CSC instead of flat_map
    This avoids O(log n) lookup.

--- include/sleipnir/autodiff/hessian_expression_graph.hpp
+++ include/sleipnir/autodiff/hessian_expression_graph.hpp
@@ -96,8 +96,12 @@ class HessianExpressionGraph {
       }
 
       // Sort parent index before child index
-      m_top_list[std::min(j, k)]->hessian_expr[std::max(j, k)] +=
-          std::move(value);
+      auto& h = m_top_list[std::min(j, k)]->hessian_expr;
+      if (!h.empty() && h.back().first == std::max(j, k)) {
+        h.back().second += std::move(value);
+      } else {
+        h.emplace_back(std::max(j, k), std::move(value));
+      }
     };
 
     auto ptr_1 = constant_ptr(Scalar(1));
@@ -254,14 +258,14 @@ class HessianExpressionGraph {
         if constexpr (UpLo == Eigen::Lower) {
           // In lower triangle, row index ≥ column index
           if (row > col) {
-            H[row, col] = value;
+            H[row, col] += value;
           } else {
-            H[col, row] = value;
+            H[col, row] += value;
           }
         } else {
-          H[row, col] = value;
+          H[row, col] += value;
           if (row != col) {
-            H[col, row] = value;
+            H[col, row] += value;
           }
         }
       }
@@ -340,7 +344,12 @@ class HessianExpressionGraph {
       }
 
       // Sort parent index before child index
-      m_top_list[std::min(j, k)]->hessian[std::max(j, k)] += value;
+      auto& h = m_top_list[std::min(j, k)]->hessian;
+      if (!h.empty() && h.back().first == std::max(j, k)) {
+        h.back().second += value;
+      } else {
+        h.emplace_back(std::max(j, k), value);
+      }
     };
 
     // Set root node's adjoint to 1 since df/df is 1
